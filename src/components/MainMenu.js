2a9cd5d27c68abda197abc593db261ec950467db
Kevin <kevin.yi93@gmail.com>
2020-10-22 17:18:59 -0400

    Create a MainMenu component with templates

---
+++ src/components/MainMenu.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+const templates = [
+  {
+    title: 'Wedding',
+    imgSrc: require('../assets/images/wedding.jpg')
+  },
+  {
+    title: 'Birthday',
+    imgSrc: require('../assets/images/birthday.jpg')
+  },
+  {
+    title: 'Graduation',
+    imgSrc: require('../assets/images/graduation.jpg')
+  },
+  {
+    title: 'Apocalypse',
+    imgSrc: require('../assets/images/apocalypse.jpg')
+  }
+]
+
+class MainMenu extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default MainMenu
