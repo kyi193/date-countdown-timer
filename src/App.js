c55c75e606ac0379354e9752f950b98a9cc8248d
Kevin <kevin.yi93@gmail.com>
2020-10-27 15:50:37 -0400

    Wrap app in Redux provider

--- src/App.js
+++ src/App.js
@@ -5,21 +5,28 @@ import DateTimeInputField from './components/DateTimeInputField'
 import MainMenu from './components/MainMenu'
 import CountdownPage from './components/CountdownPage'
 import { HashRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import reducer from './reducers'
+import middleware from './middleware'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <header className="App-header">
-            <div>
-              <Route exact path='/' component={CountdownPage} />
-              <Route exact path='/home' component={MainMenu} />
-              <Route exact path='/templates/:templateID' component={DateTimeInputField} />
-            </div>
-          </header>
-        </Router>
-      </div>
+      <Provider store={createStore(reducer, composeWithDevTools(middleware))}>
+        <div className="App">
+          <Router>
+            <header className="App-header">
+              <div>
+                <Route exact path='/' component={CountdownPage} />
+                <Route exact path='/home' component={MainMenu} />
+                <Route exact path='/templates/:templateID' component={DateTimeInputField} />
+              </div>
+            </header>
+          </Router>
+        </div>
+      </Provider>
     );
   }
 }
